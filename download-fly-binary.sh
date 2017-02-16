55cb5cbc5b9c47914ea04082edc2c3805a683627
Jim Lin <jimlin@cloudcube.com.tw>
2017-02-16 00:41:16 -0500

    update download-fly-binary.sh

--- download-fly-binary.sh
+++ download-fly-binary.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://github.com/concourse/concourse/releases/download/v2.4.0/fly_darwin_amd64 -O fly
+wget https://github.com/concourse/concourse/releases/download/v2.4.0/fly_$(uname)_amd64 -O fly
 chmod +x fly
 ./fly --version
